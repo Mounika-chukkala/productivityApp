91ef1bdd1f3bb048c5885b7f1c1fed5c7525152c
Mounika Ch <126870495+Mounika-chukkala@users.noreply.github.com>
2025-08-02 18:17:28 +0530

    Update AuthForm.jsx

--- frontend/src/pages/AuthForm.jsx
+++ frontend/src/pages/AuthForm.jsx
@@ -67,7 +67,7 @@ function AuthForm({ type }) {
   return (
     <motion.div
       key={location.pathname}
-      className="h-screen relative bg-[#FFFFFF] flex justify-center items-start py-5 px-4 font-sans text-[#2F3E2F]"
+      className="h-[calc(100vh-100px)] relative bg-[#FFFFFF] flex justify-center items-start py-5 px-4 font-sans text-[#2F3E2F]"
     >
        <div className=" absolute lg:right-15 lg:bottom-10 bottom-0 right-2">
       <p className="text-sm text-slate-800">Sample login details: </p>
